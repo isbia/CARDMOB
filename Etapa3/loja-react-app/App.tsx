488007de7aaf72df94dc4264cdbd30b4968bf792
Ana Beatriz <ana.melo1@aluno.ifsp.edu.br>
2025-08-12 10:11:43 -0300

    Navegação básica entre telas

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -2,14 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
-import HomeScreen from './src/screens/HomeScreen';
+//import HomeScreen from './src/screens/HomeScreen';
+import RootNavigator from './src/navigation/RootNavigation';
 
 export default function App() {
   return (
    <ThemeProvider>
-     <HomeScreen />
+     <RootNavigator />
    </ThemeProvider>
-   
+
   );
 }
 
