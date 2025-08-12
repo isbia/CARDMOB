e89f5b42ce4d55582fd3c92bdf7e7a9f6a7b2db2
Ana Beatriz <ana.melo1@aluno.ifsp.edu.br>
2025-08-12 08:41:34 -0300

    Inclui ThemeContext

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
+import HomeScreen from './src/screens/HomeScreen';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <ThemeProvider>
+     <HomeScreen />
+   </ThemeProvider>
+   
   );
 }
 
